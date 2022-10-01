1afdcd686d362ccbbcf7a009451c6615229bfda5
Daniel Ramirez <dani_0318@hotmail.com>
2022-09-30 22:35:07 -0500

    240 creacion del proyecto y servicio para los posts

---
+++ src/app/pages/tab1/tab1.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/app/services/posts.service';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page implements OnInit{
+
+  constructor(private postsService: PostsService) {}
+
+  ngOnInit() {
+    
+    this.postsService.getPosts().subscribe( resp => {
+      console.log(resp);
+    });
+
+  }
+
+}
---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  paginaPosts = 0;
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(){
+
+    this.paginaPosts ++;
+
+    return this.http.get(`${URL}/posts?pagina=${this.paginaPosts}`)
+
+  }
+}
